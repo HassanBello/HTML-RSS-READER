ccc5bd3e387889f445407565d9ad81a9ad6e0dc2
Modupe Bello <tigerbello5@yahoo.com>
2017-06-26 14:47:43 +0200

    Theme
    Added a button for changing the theme, to either grey, burgundy or dark
    blue. This should be my last commit unless i find something to correct.

--- assets/js/night.js
+++ assets/js/night.js
@@ -1,17 +1,41 @@
-(function toggleNightMode() {
- 
-  var backgroundColor = document.getElementById('rss').rssfeed.item.bgcolor;
-  
-    
-  if (backgroundColor == "white") {
-        //document.getElementById('mydiv').style.color="white";
-        document.getElementById('rss').rssfeed.item.bgcolor="black";
-        //document.getElementById('mydiv').style.fontFamily="Arial";
-        
-    } else {
-       /* document.getElementById('mydiv').style.color="black"; */
-        document.getElementById('rss').rssfeed.item.bgcolor="white";
-//        document.getElementById('mydiv').style.fontFamily="Times New Roman";
+     function day() {
+    body.className = "lightSwitch";
+};
+
+function night() {
+    body.className = "darkSwitch";
+};
+
+function reset() {
+    body.className = "";
+};
+
+$(function () {
+    /* RegEx to grab the "bgColor" cookie */
+    var bgColor = document.cookie.replace(/(?:(?:^|.*;\s*)bgColor\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+    var button = $('input[type=button].changeBg');
+    button.on('click', function (event) {
+        event.preventDefault();
+
+        /* Executing the function associated with the button */
+        eval($(this).val().toLowerCase() + "();");
+
+        button.not($(this)).removeAttr('disabled');
+        if ($(this).val() != "Reset") {
+            $(this).attr('disabled', '');
+
+            /* Here we create the cookie and set its value, does not happen if it's Reset which is fired. */
+            document.cookie = "bgColor="+$(this).val();
+        }
+    });
+
+    /* If the cookie is not empty on page load, execute the function of the same name */
+    if(bgColor.length > 0)
+    {     
+        eval(bgColor.toLowerCase()+'()');
+
+        /* Disable the button associated with the function name */
+        $('button[value="'+bgColor+'"]').attr("disabled","disabled");
     }
-}
-) (jquery);
+});
